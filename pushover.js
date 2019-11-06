2364a7e896a834abbd049e2c775bcff634fbd21b
maxwroc <max.chodorowski@gmail.com>
2019-11-06 22:44:00 +0000

    pushover

---
+++ pushover.js
@@ -0,0 +1,34 @@
+
+const request = require("request");
+
+const notify = (config, msg, title) => {
+
+    if (!config.pushover || !config.pushover.token || !config.pushover.user) {
+        throw new Error("Missing pushover configuration");
+    }
+
+    if (config.test) {
+        console.log(title, msg);
+        return;
+    }
+
+    request.post("https://api.pushover.net/1/messages.json", {
+            json: {
+                token: config.pushover.token,
+                user: config.pushover.user,
+                message: msg,
+                title: title
+            }
+        },
+        (error, res, body) => {
+            if (error) {
+                console.error(error);
+                return;
+            }
+            console.log(`statusCode: ${res.statusCode}`);
+            console.log(body);
+        }
+    );
+}
+
+exports.notify = notify;
